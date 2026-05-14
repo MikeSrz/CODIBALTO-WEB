48029a57ea79197ff394dff34dad9e550c63136d
Michael Suárez <onriosrz@gmail.com>
2026-05-14 20:22:06 +0200

    Completando flujo de funciones de encriptacion y desencriptacion.

--- src/services/crypto.ts
+++ src/services/crypto.ts
@@ -1,15 +1,15 @@
 import { INFO_AUTH, INFO_ENCRYPT } from "../constants";
 /*
- Este servicio se encarga de manejar operaciones criptográficas.
-    - Generar sal aleatoria
-    - Derivar Contraseña a MasterKey usando PBKDF2
-    - Derivar MasterKey a Clave de cifrado EncKey usando HKDF
-    - Derivar MasterKey a Clave de autenticación AuthKey usando HKDF
-    - Cifrar y Descifrar datos usando AES-GCM
+ Este servicio se encarga de manejar operaciones criptográficas con Web Crypto API.
+    - Generar sal aleatoria ~Listo
+    - Derivar Contraseña a MasterKey usando PBKDF2 ~Listo
+    - Derivar MasterKey a Clave de cifrado `EncKey` usando HKDF ~Listo
+    - Derivar MasterKey a Clave de autenticación `AuthKey` usando HKDF ~Listo
+    - Cifrar y Descifrar datos usando AES-GCM ~Pendiente ~Listo
 */
 
 
-export function generateSalt(length: number = 16): Uint8Array {
+export function generateSalt(length: number = 16): Uint8Array { //Se genera un salt por usuario, en cada registro.
     return crypto.getRandomValues(new Uint8Array(length));
 }
 
@@ -32,7 +32,7 @@ export async function derivateMasterPassword(password: string, salt: Uint8Array,
         },
         baseKey,
         256
-    );
+        );
 
     const masterKey = await crypto.subtle.importKey(
         'raw',
@@ -44,7 +44,7 @@ export async function derivateMasterPassword(password: string, salt: Uint8Array,
     return masterKey;
 }
 
-export async function derivateMKey(masterKey: CryptoKey, info: string): Promise<CryptoKey> { // esta función deriva una clave de cifrado a partir de la clave maestra usando HKDF2
+export async function derivateMKey(masterKey: CryptoKey, info: string): Promise<CryptoKey> { // esta función deriva una clave de cifrado o un auth_hash a partir de la clave maestra usando HKDF2
     const infoCoded = new TextEncoder().encode(info);
     const key = await crypto.subtle.deriveBits(
          {
@@ -54,6 +54,7 @@ export async function derivateMKey(masterKey: CryptoKey, info: string): Promise<
          },
             masterKey,
     );
+    //controlamos segun el parámetro que se le pase si se trata de la clave de cifrado o de la clave de autenticación, para importarla con el algoritmo correcto
     let keyResult : CryptoKey;
     switch(info) {
         case INFO_ENCRYPT:
@@ -82,6 +83,35 @@ export async function derivateMKey(masterKey: CryptoKey, info: string): Promise<
 }
 
 
-export async function encryptData(data: string ) {
-    return 0;
+
+
+export async function encryptData(data: string,encKey: CryptoKey) : Promise<{cyphertext: ArrayBuffer, iv: Uint8Array}> { //retorna un objetco con el texto cifrado y el IV utilizado para el cifrado. Iv es vital para descrifrado.
+    const encoder = new TextEncoder();
+    const dataBytes = encoder.encode(data);
+    const iv : Uint8Array = crypto.getRandomValues(new Uint8Array(12)); // AES-GCM recomienda un IV de 12 bytes
+
+    const cyphertext = await crypto.subtle.encrypt(
+        {
+            name: 'AES-GCM',
+            iv: iv.buffer as ArrayBuffer
+        },
+        encKey,
+        dataBytes
+    );
+   
+    return {cyphertext: cyphertext, iv: iv};
+}
+
+
+export async function decryptData(cyphertext: ArrayBuffer, iv: Uint8Array, encKey: CryptoKey) : Promise<string> { // esta función descifra los datos cifrados usando AES-GCM y el IV utilizado para el cifrado
+    const decoder = new TextDecoder(); //Para decodificar el resultado.
+    const decryptedBytes = await crypto.subtle.decrypt(
+        {
+            name: 'AES-GCM',
+            iv: iv.buffer as ArrayBuffer
+        },
+        encKey,
+        cyphertext
+    );
+    return decoder.decode(decryptedBytes);
 }
