eb48a651f3463b3a285d73dba84563737f5b2817
Michael Suárez <onriosrz@gmail.com>
2026-05-28 22:38:00 +0200

    Añadiendo home, tailwind y enrutamiento.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import Home from '../views/Home.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [ 
+    {path: '/home', component: Home}
+  ],
 })
 
 export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,4 @@
+<template>
+    <img src="../assets/LogoLight.svg" alt="Logo"/>
+    <h1 class="bg-stone-900 text-white min-h-screen">Bienvenido</h1>
+</template>
